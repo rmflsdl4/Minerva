0a95c2e3187275ced04639df27d4ca6f5d391ea0
rmflsdl4 <rmflsdl4@gmail.com>
2023-11-28 00:44:49 +0900

    Update app.js

--- app.js
+++ app.js
@@ -182,6 +182,7 @@ async function RecusionRequest(cnt){
         try{
             const bookData = await database.Query(sql, tempData);
             console.log(bookData[0]);
+            isbnData = null;
             return bookData[0];
         }
         catch(err){
